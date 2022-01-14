ec27e3d93d04e8cc3c4e4f3f75055cf226605148
ScarfaceHBC <86946929+ScarfaceHBC@users.noreply.github.com>
2022-01-14 08:30:47 +0100

    ...

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -16,7 +16,7 @@ export const LoginScreen = () => {
       "713405592516-ol60pnhss6aslg9e9cpebqfc1ec2uc15.apps.googleusercontent.com",
     iosClientId: "GOOGLE_GUID.apps.googleusercontent.com",
     androidClientId:
-      "713405592516-niil52s930ouj256enoemn2drdranikk.apps.googleusercontent.com",
+      "713405592516-u5sc7u9r62tq01ctj9ifmhn7btfp3e0m.apps.googleusercontent.com",
     webClientId: "GOOGLE_GUID.apps.googleusercontent.com",
     //###################
   });
