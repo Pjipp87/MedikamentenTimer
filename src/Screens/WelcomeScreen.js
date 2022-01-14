7c480e43ca58c691d0a68e608bf1ba6c96b1ad2a
ScarfaceHBC <86946929+ScarfaceHBC@users.noreply.github.com>
2022-01-14 19:21:03 +0100

    ....

--- src/Screens/WelcomeScreen.js
+++ src/Screens/WelcomeScreen.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export const WelcomeScreen = () => {
+  const [userName, setUserName] = useState("");
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUserName(user.displayName);
+        /**
+          * _getStatusUpdate();
+          setCurrentUsernameFunc(user.email);
+          setCurrentUserData({
+            name: user.displayName,
+            username: user.email,
+            picture: user.photoURL,
+            id: user.uid,
+          });
+  
+          _setFriendOnline(user);
+          _setUserList(user);
+           */
+      } else {
+        // User is signed out
+        // ...
+      }
+    });
+  }, [user]);
+
   return (
     <View style={styles.main}>
-      <Text>Willkommen</Text>
+      <Text>Willkommen {userName}</Text>
     </View>
   );
 };
