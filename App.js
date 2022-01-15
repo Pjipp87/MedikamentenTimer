3b2ba2883d0d85303b674fd80d82f18ad1308fca
ScarfaceHBC <p.jipp87@gmail.com>
2022-01-15 15:27:32 +0100

    ...

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import MainController from "./src/controller/mainController";
 
 export default function App() {
-  return <MainController></MainController>;
+  return (
+    <>
+      <MainController></MainController>
+      <StatusBar />
+    </>
+  );
 }
