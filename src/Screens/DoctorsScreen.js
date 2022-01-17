62a91e76b7bfa7550dde4f05ad2fb79bca1f0346
pascal <p.jipp87@gmail.com>
2022-01-17 17:37:23 +0100

    ....

--- src/Screens/DoctorsScreen.js
+++ src/Screens/DoctorsScreen.js
@@ -1,11 +1,65 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Button } from "react-native-paper";
+import { StyleSheet, View, FlatList, useWindowDimensions } from "react-native";
+import { Button, Text, Headline, Title } from "react-native-paper";
 
 export const DoctorsScreen = ({ navigation }) => {
+  const doctors = [
+    {
+      name: "Konofsyk",
+      specialist: "Augenarzt",
+      street: "Karlstraße 2/4",
+      zipcode: "95028",
+      city: "Hof",
+      tel: "09281 1008",
+      id: 1,
+    },
+    {
+      name: "Kücüköztürk",
+      specialist: "Hausarzt",
+      street: "Gabelsbergerstraße 45",
+      zipcode: "95028",
+      city: "Hof",
+      tel: "09281 84722",
+      id: 2,
+    },
+    {
+      name: "Swoboda",
+      specialist: "Allgemeinmedizin",
+      street: "Königstraße 19",
+      zipcode: "95028",
+      city: "Hof",
+      tel: "09281 3861",
+      id: 3,
+    },
+    {
+      name: "Dietrich",
+      specialist: "Kinderarzt",
+      street: "Luitpoldstraße 19",
+      zipcode: "95028",
+      city: "Hof",
+      tel: "09281 3816",
+      id: 4,
+    },
+  ];
+
+  const ListItem = ({ item }) => {
+    return (
+      <View style={{ marginVertical: 15 }}>
+        <Headline>{`${item.name}`}</Headline>
+        <Text>{`${item.specialist}`}</Text>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.main}>
       <Text>Ärzte</Text>
+      <FlatList
+        data={doctors}
+        renderItem={({ item }) => <ListItem item={item} />}
+        keyExtractor={(item) => item.id}
+        style={{ width: useWindowDimensions().width, paddingHorizontal: 25 }}
+      />
       <Button onPress={() => navigation.navigate("Willkommen")}>Zurück</Button>
     </View>
   );
